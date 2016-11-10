801889da8993a3e5b4b60f974beefeb0f043e17f
Nicolas Gonzalez <nicolasnicogonzalez@gmail.com>
2016-11-10 18:54:54 -0500

    linear interpolation
    code now uses linear interpolation to create smooth gradients over the
    HSV scale.

--- mandelbrot.fs.glsl
+++ mandelbrot.fs.glsl
@@ -3,6 +3,16 @@ precision highp float;
 uniform vec2 viewportDimensions;
 uniform vec4 bounds; // minI, maxI, minR, maxR
 
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+float interpolate(float v0, float v1, float t) {
+    return (1.0 - t) * v0 + t * v1;
+}
+
 void main() {
     // [-2.0, 2.0]
     vec2 c = vec2(
@@ -28,7 +38,15 @@ void main() {
         iterations += 1.0;
     }
 
-    float quotient = (iterations / maxIterations);
-    float fraction = mod(quotient, 1.0) * (maxIterations / 100.0);
-    gl_FragColor = vec4(fraction, fraction, fraction, 1.0);
+    if (iterations < maxIterations ) {
+        float hsq = z.x*z.x + z.y*z.y;
+        float log_zn = log(hsq) / 2.0;
+        float nu = log(log_zn / log(2.0)) / log(2.0);
+
+        iterations = iterations + 1.0 - nu;
+    }
+
+    float color1 = floor(iterations) / maxIterations;
+    float color2 = (floor(iterations) + 1.0) / maxIterations;
+    gl_FragColor = vec4(hsv2rgb(vec3(interpolate(color1, color2, mod(iterations, 1.0)), 1.0, 1.0)), 1.0);
 }
